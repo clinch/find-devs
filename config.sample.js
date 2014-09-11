c59ed9764fb4130c43bb1d3c03fb1afd2370f31c
Chris Inch <github@chrisinch.com>
2014-09-11 14:50:51 -0400

    Some files needed in order to specify the config constants

---
+++ config.sample.js
@@ -0,0 +1,12 @@
+
+/**
+ * Use this config sample to configure find-devs
+ * These constants will vary from user to user.
+ */
+
+var config = {};
+
+// Goto https://github.com/settings/tokens/new to get yourself your own tokens.
+config.GITHUB_TOKEN  = '123abc';
+
+module.exports = config;
